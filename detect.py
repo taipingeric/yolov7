6d26487d1d2011f54afe7a62c05117e88ebd2180
SAI <taipingeric@gmail.com>
2023-03-12 11:38:25 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -59,7 +59,7 @@ def detect(save_img=False):
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
 #     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
-    colors = [[0, 255, 0], [0, 255, 255], [255, 0, 0], [255, 0, 255]]
+    colors = [[0, 255, 0], [255, 255, 0], [255, 0, 0], [255, 0, 255]]
 
     # Run inference
     if device.type != 'cpu':
